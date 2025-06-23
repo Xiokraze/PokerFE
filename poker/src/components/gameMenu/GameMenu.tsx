a56364905985e462ce6a3e313c6c998e12958c77
MSI\jwort <xiokraze@gmail.com>
2025-06-22 23:10:55 -0400

    Continued polishing the game menu.

--- poker/src/components/gameMenu/GameMenu.tsx
+++ poker/src/components/gameMenu/GameMenu.tsx
@@ -10,7 +10,10 @@ const GameMenu: React.FC = () => {
   return (
     <div className={styles.grid}>
       {games.map(
-        ({ id, displayName, description, cardCode, badgeText, badgeColor, enabled, routePath }) => (
+        (
+          { id, displayName, description, cardCode, badgeText, badgeColor, enabled, routePath },
+          index,
+        ) => (
           <div
             key={id}
             className={`${styles.cardContainer} ${enabled ? '' : styles.disabled}`}
@@ -21,10 +24,13 @@ const GameMenu: React.FC = () => {
               if (enabled && (e.key === 'Enter' || e.key === ' ')) navigate(routePath);
             }}
             aria-disabled={!enabled}
+            style={{ '--animation-delay': `${index * 200}ms` } as React.CSSProperties} // stagger by 200ms
           >
             <PlayingCard code={cardCode} badgeText={badgeText} badgeColor={badgeColor} />
-            <h3 className={styles.gameName}>{displayName}</h3>
-            <p className={styles.gameDescription}>{description}</p>
+            <div className={styles.text}>
+              <h3>{displayName}</h3>
+              <p>{description}</p>
+            </div>
           </div>
         ),
       )}
